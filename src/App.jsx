01152c1462b598da1dbe7c4b8963a9ebed609821
Minkaill <mkx.dake@gmail.com>
2023-05-14 03:04:12 +0300

    Подключил redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Header } from "./components/Header";
+
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
